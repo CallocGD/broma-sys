9a5e40b274cb3a86c104e4d2675b16e73190f557
Calloc <140273795+CallocGD@users.noreply.github.com>
2024-12-11 12:35:16 -0600

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,42 +1,39 @@
 
 extern crate cc;
+// extern  crate 
 use bindgen::builder;
 
 
 fn main(){
-    let bind = builder()
-        .header("broma/include/ast.hpp")
-        .header("broma/include/broma.hpp")
-        .clang_args(&["--std=c++17"])
+    let manifest_dir = std::path::Path::new(env!("CARGO_MANIFEST_DIR"));
 
+    let bind = builder()
+        .header("cbroma/cbroma.h")
         .generate().unwrap();
+
     bind.write_to_file(std::path::PathBuf::from(
         std::env::var("OUT_DIR").unwrap()
     ).join("bindings.rs")).unwrap();
 
-
-    let manifest_dir = std::path::Path::new(env!("CARGO_MANIFEST_DIR"));
-    // let ghc = manifest_dir.join("ghc");
     let mut build = cc::Build::new();
-    // build.cpp(true);
+    build.cpp(true);
     build.flag_if_supported("-std=c++17");
     build.flag_if_supported("/std:c++17");
     build.flag_if_supported("-fexceptions");
-    build.flag_if_supported("/fexceptions");
     build.flag_if_supported("/EHsc");
 
-    // // just so we can reach ghc. That's fucked up and I spent 2 hours just figuring that out.
+    // just so we can reach ghc. That's fucked up and I spent 2 hours just figuring that out.
     build.include(manifest_dir);
     build.include(manifest_dir.join("broma").join("include"));
     build.include(manifest_dir.join("broma").join("PEGTL-3.2.7").join("include"));
     // build.include(manifest_dir.join("broma").join("PEGTL-3.2.7").join("include").join("tao").join("pegtl"));
     // build.include(manifest_dir.join("broma").join("PEGTL-3.2.7").join("include").join("tao").join("pegtl").join("contrib"));
+    // build.include(manifest_dir.join("cbroma"));
+
     build.file(manifest_dir.join("broma/src/broma.cpp"));
+    // Compile C Port so Rust knows how to use broma correctly...
+    build.file(manifest_dir.join("cbroma/cbroma.cpp"));
+    build.include(manifest_dir.join("cbroma"));
     build.compile("broma");
-
-    
-    
-
-
-
 }
+
